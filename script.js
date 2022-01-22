25050faeb81b477be1cae995cf7dab8108171cb4
ymalkawi <malkawi.chicago@gmail.com>
2022-01-21 23:22:45 -0600

    this is code convertor projects

---
+++ script.js
@@ -0,0 +1,5 @@
+    function input(){
+                  var name1 = document.getElementById("textarea").value;
+                  document.getElementById("output").innerHTML = name1;
+                  
+              }
